300a193c4846d2bd9b290af1048cc85d14aa2f00
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-08 16:41:25 -0500

    1

---
+++ Escape_sequences.py
@@ -0,0 +1,12 @@
+# fmt: off
+course = "python \"programming\""
+# fmt: on
+# (also used fmt: off and fmt:on so as to not have black or pep8 rework the syntax)
+# we used a backslash to 'escape' using the double quote, so it sees it as text and not code
+print(course)
+
+# here are the escape sequences you should know about
+# \"
+# \'
+# \\
+# \n (for new line)
