66bbfcbe937b4d58986856d288685effd0561d07
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-08 16:48:24 -0500

    1

--- Formatted_strings.py
+++ Formatted_strings.py
@@ -3,3 +3,6 @@ last = "Dunthorne"
 full = first + " " + last
 # when you add variables together in this way its called concatenation
 print(full)
+
+# f strings or formatted strings are a better way to concatenate this kind of data
+print(f"{first} {last}")
