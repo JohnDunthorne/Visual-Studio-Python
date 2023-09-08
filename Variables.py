e2c6f74f7be555cabca54a8f82121f4c163feaae
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-08 15:51:30 -0500

    1

--- Variables.py
+++ Variables.py
@@ -1,1 +1,18 @@
-print(1 + 2)
+# variables are objects with assigned values
+students_count = 1000
+# this is a variable, this stores data in a memory location
+
+print(students_count)
+
+# you can store numbers (int or float etc.) text (str) or boolean (True or False)
+student_name = "John"
+# this is a (str)
+student_age = 42
+# this is ant (int)
+student_weight = 136.6
+# this is a (float) - a non integer
+student_enrolled = True
+# this is a boolean
+print(
+    f"{student_name} is {student_age} years old, they weigh {student_weight} lbs and their enrollment status is currently {student_enrolled}"
+)
