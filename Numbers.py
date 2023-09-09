d31756ce9ff1065568d5fc3a94fe5e7d82904c97
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 08:18:32 -0500

    1

---
+++ Numbers.py
@@ -0,0 +1,24 @@
+# there are 3 types of numbers python deals with, integer, float and complex numbers
+
+# integers are whole numbers
+x = 1
+
+# floats are numbers with decimal points
+y = 2.3
+
+# complex numbers are something more common with engineers
+z = 1 + 2j  # j being an imaginary number (like i in mathematics)
+
+print(f"{x} {y} {z}")
+
+# python contains all the standard aritmatic operations that we use in math
+
+a = 1 + 2  # addition
+b = 2 - 1  # subtraction
+c = 3 * 3  # multiplication
+d = 16 / 2  # division
+e = 16 // 2  # double slash will return an integer automatically
+
+print(f"{a} {b} {c} {int(d)}")
+# i wrote int(d) so the number gets represented by an integer rather than a float
+print(e)
