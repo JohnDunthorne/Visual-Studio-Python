52ab003d4900c3a7fb0a31a04075fb65bc5f953e
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 11:23:28 -0500

    1

--- Control_flow/Logical_operators.py
+++ Control_flow/Logical_operators.py
@@ -13,3 +13,16 @@ if high_income or good_credit:
 else:
     print("Not eligible")
 # this one will return eligible, since one of the two conditions are 'True'
+
+student = True
+if student:
+    print("Eligible")
+elif not student:
+    print("Ineligible")
+# if student was marked False, this would return Ineligible
+
+if (high_income or good_credit) and student:
+    print("Eligible")
+else:
+    print("Ineligible")
+# this would return Eligible, since one of the first two conditions is 'True', and student is 'True'
