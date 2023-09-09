7467e9ce8b1c78d31575cf96cc36ee24d794ba20
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 11:56:05 -0500

    1

---
+++ Control_flow/For_loops.py
@@ -0,0 +1,19 @@
+print("sending a message")
+print("sending a message")
+print("sending a message")
+# this is ugly code, to repeat a task use a 'for' loop
+
+for number in range(3):  # we are passing 3 as an argument
+    print("Attempt", number)
+# number will return each attempt index, in this instance 0, 1 and 2
+
+# to make this more user friendly we can write like this:
+for number in range(3):
+    print("Attempt", number + 1)
+# which will return attempts as 1, 2 and 3
+
+# to avoid this we could also write
+for number in range(1, 4):
+    print("Attempt", number, number * ".")
+# this ultimately returns the same result without having to type number + 1
+# also added number * "." to add that many dots after statement. just for fun
