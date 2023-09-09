65651ccd479f47e3910ea26527e4e9dc31945d5b
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 08:47:17 -0500

    1

---
+++ Number_functions.py
@@ -0,0 +1,12 @@
+import math
+
+# use this to import the math module so you can use math . (dot) methods for example
+print(
+    "math.ceil returns the ceiling of the number, in this case 2.6 returns: "
+    + str(math.ceil(2.6))
+)
+print("you can look up python math module on google to see what all the functions do")
+print("the round function is rounding 3.6 which is: " + str(round(3.6)))
+# round will round your number to the nearest integer
+print("the abs function returns the absolute value of -2.9 which is: " + str(abs(-2.9)))
+# abs returns absolute value
