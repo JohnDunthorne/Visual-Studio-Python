45acbbaeaa1856ee71193fa5a3ebde71d3e18c7e
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 16:51:33 -0500

    1

--- Control_flow/For_loops.py
+++ Control_flow/For_loops.py
@@ -17,3 +17,8 @@ for number in range(1, 4):
     print("Attempt", number, number * ".")
 # this ultimately returns the same result without having to type number + 1
 # also added number * "." to add that many dots after statement. just for fun
+
+for number in range(1, 20, 3):
+    print("Attempt", number, number * ".")
+# when dealing with ranges you can specify a step as well (3rd number in brackets), in this case
+# it will list numbers between 1 and 19 (the 1, 20 in the brackets), but only every third
