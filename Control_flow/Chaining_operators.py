01a2f168ba3e1aaf147cc542fe9855b27e9649b6
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 11:30:08 -0500

    1

---
+++ Control_flow/Chaining_operators.py
@@ -0,0 +1,9 @@
+# age should be between 18 and 65
+age = 22
+if age >= 18 and age < 65:
+    print("Eligible")
+
+# this can be simplified by writing
+if 18 <= age < 65:
+    print("eligible")
+# this is called chaining comparison operators
