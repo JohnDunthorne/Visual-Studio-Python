bcd65263cd2466295b70a8ec5da7c765dba57c18
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 09:45:23 -0500

    1

---
+++ Control_flow/Comparison_operators.py
@@ -0,0 +1,14 @@
+print(10 > 3)  # this represents 10 being greater than 3
+# this will return True as this is a boolean expression
+print(10 >= 3)  # this is greater than or equal to, also True
+print(10 < 20)  # 10 is less than 20, also True
+print(10 <= 20)  # less than or eqial to 20, also True
+# fmt: off
+print(10 == 20)  # equal to, this one would be False (equality opertor is 2 equal signs ==)
+print(10 == "10")  # this would be False as the computer see str and int different, so these two are technically to equal
+# fmt: on
+print(10 != 10)  # this is NOT equal !=, hence this would be a False statement
+
+# we can do this on strings as well, for example
+print("bag" > "apple")  # this returns true since bag come after apple alphabetically
+print("bag" == "BAG")  # this would return false as its case sensitive
