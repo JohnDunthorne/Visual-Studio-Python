2c51777d824a5d933a374a1c67db112efb837558
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-09 08:54:46 -0500

    1

---
+++ Type_conversion.py
@@ -0,0 +1,10 @@
+x = input("x: ")
+# this will crate a viriable based on user input for x
+y = x + 1
+# this will error at run as the x input will be a str and y is adding a int to it it
+
+# we can type convert inputs to different things for example
+int(x)  # to convert x to an integer
+float(x)  # to convert x to float
+bool(x)  # to convert x to a boolean
+str(x)  # to convert x to a string
