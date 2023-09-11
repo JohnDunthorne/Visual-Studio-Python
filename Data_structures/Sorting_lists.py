b791f9aaa76730d5b3d735f82f3991e3b4b8cfa2
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-11 16:31:56 -0500

    1

--- Data_structures/Sorting_lists.py
+++ Data_structures/Sorting_lists.py
@@ -17,3 +17,28 @@ print(sorted(numbers))  # < they are sorted here
 print(numbers)  # < but still intact
 print(sorted(numbers, reverse=True))  # < now sorted backwards
 print(numbers)  # < but original list preserved
+
+print("-" * 60)
+# sorting complex lists:
+
+items = [
+    ("product3", 30),
+    ("product1", 10),
+    ("product2", 20),
+    ("product5", 50),
+    ("product4", 40),
+]
+print(items)
+
+
+# we can make our own function to sort this list
+def sort_item(item):
+    return item[1]
+
+
+# now when you call the sort function, and use this function as a reference
+# it will try to sort the index of the second item in each tuple (the price)
+# so it should sort them by price now
+
+items.sort(key=sort_item)  # key= since sort takes no positional arguements
+print(items)
