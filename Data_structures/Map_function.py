a7c243b995ce2f75552209e2d2ee7140c029bf63
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-12 14:46:36 -0500

    1

--- Data_structures/Map_function.py
+++ Data_structures/Map_function.py
@@ -15,3 +15,22 @@ for item in items:
     )  # < this will append the second object in each tuple (the price)
 
 print(prices)
+
+items2 = [
+    ("product3", 31),
+    ("product1", 12),
+    ("product2", 23),
+    ("product5", 54),
+    ("product4", 45),
+]
+
+# alternatively we can use the map function to do this
+
+newitems = map(lambda item: item[1], items2)
+# < items2 is here as we are taking the item[1] from items 2
+for item in newitems:
+    print(item)
+
+# this can also be written as a list like this
+newitems2 = list(map(lambda item: item[1], items2))
+print(newitems2)
