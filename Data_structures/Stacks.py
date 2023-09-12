b1eafe7c47d3cd1ff36ec40cb753e28823f0c979
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-12 15:19:14 -0500

    1

---
+++ Data_structures/Stacks.py
@@ -0,0 +1,6 @@
+# LIFO means last in first out, we will use it in stacks
+
+browsing_session = []
+browsing_session.append(1)  # 1 is the first website we are visiting
+browsing_session.append(2)  # second website
+browsing_session.append(3)  # third website
