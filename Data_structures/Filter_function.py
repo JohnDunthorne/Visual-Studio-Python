e26953b93664c3068a0f4b755a68d7ff3bfea20b
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-12 14:56:38 -0500

    1

--- Data_structures/Filter_function.py
+++ Data_structures/Filter_function.py
@@ -1,3 +1,5 @@
+# we want to create a filter that returns numbers only 30 or more in price
+
 items = [
     ("product3", 30),
     ("product1", 10),
@@ -5,3 +7,17 @@ items = [
     ("product5", 50),
     ("product4", 40),
 ]
+# we could do it like this:
+filtered = []
+for item in items:
+    if item[1] >= 30:
+        print(item)
+
+# but there is a built in filter function
+price_30_or_above = filter(lambda item: item[1] >= 30, items)
+print(price_30_or_above)
+# this code does exactly what we did above without the for loop
+
+# we can put them straight in a list too
+price_30_or_above = list(filter(lambda item: item[1] >= 30, items))
+print(price_30_or_above)
