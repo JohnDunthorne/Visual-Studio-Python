af5627cd035dad05c40f5a027f9bceff3fd21bc7
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-13 16:02:35 -0500

    1

---
+++ Data_structures/Swapping_variables.py
@@ -0,0 +1,16 @@
+x = 10
+y = 11
+
+# we can swap these two variables like this:
+
+x, y = y, x
+print(x, y)
+# ***11 10*** is returned
+
+# We can use this same methodology to define multiple variables on one line
+a, b, c, d = 1, 2, 3, 4
+print(a)
+print(b)
+print(c)
+print(d)
+# ***1 2 3 4*** is returned
