04926c63debf89011efb07079b427dff714a768f
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-14 17:37:12 -0500

    1

---
+++ Data_structures/Unpacking_operator.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+print(numbers)
+
+# this returns ***[1, 2, 3]***
+
+# what if we want to have 1 2 3 be displayed we use the unpacking operator
+
+numbers = [1, 2, 3]
+print(*numbers)
+
+# simply put a * infront of the list you are printing
