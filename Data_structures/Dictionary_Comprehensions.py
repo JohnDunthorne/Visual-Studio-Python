92bf1230cd0017304a6082cc201eb3f39199cbc0
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-14 17:08:25 -0500

    1

--- Data_structures/Dictionary_Comprehensions.py
+++ Data_structures/Dictionary_Comprehensions.py
@@ -2,3 +2,19 @@ values = []
 for number in range(5):
     values.append(number * 2)
 print(values)
+
+# so above here we create an empty list called values
+# we then do a for loop, for numbers in a range of 5
+# we append the nummber, multiplied by 2
+# this returns ***[0, 2, 4, 6, 8]***
+
+# we can use list comprehension to phrase this as
+# [expression for item in items]
+# the expression is - (number * 2)
+# the item is >number<
+# and the items is >range(5)<
+
+values = [number * 2 for number in range(5)]
+print(values)
+
+# this will return an identical list to that of above
