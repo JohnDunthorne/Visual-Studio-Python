eb27230655f9ffb044dcf8d6b17e9ab03b589c51
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-15 12:50:05 -0500

    1

---
+++ Classes/Classes.py
@@ -0,0 +1,14 @@
+# a class is a blueprint to create a new object
+
+x = 1
+print(type(x))
+y = "hello"
+print(type(y))
+z = 4.567
+print(type(z))
+alpha = True
+print(type(alpha))
+beta = ""
+print(type(beta))
+
+# using the above method you can see what class a variable is
