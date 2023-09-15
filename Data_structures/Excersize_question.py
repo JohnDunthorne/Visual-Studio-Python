db86f71d982a4a41e5f976278080402124d637a3
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-15 06:24:49 -0500

    1

---
+++ Data_structures/Excersize_question.py
@@ -0,0 +1,14 @@
+# you can add pretty printing
+from pprint import pprint
+
+sentence = "This is a common interview question"
+# find the most repeated letter in this text
+
+char_frequency = {}  # < dictionary
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+
+pprint(char_frequency, width=1)  # pprint is a good way to print dictionaries
