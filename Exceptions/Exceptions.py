7cf81d610b2e7441bb9ada1baa9e21b878dd81fe
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-15 11:54:01 -0500

    1

---
+++ Exceptions/Exceptions.py
@@ -0,0 +1,10 @@
+# numbers = [1, 2]
+# print(numbers[3])
+
+# this will return an error as there is nothing at index 3
+
+age = int(input("age: "))
+
+# with the above if the user types something other than a 'int' it will cause an exception
+
+# as a programmer you need to handle exceptions so the program does not crash
